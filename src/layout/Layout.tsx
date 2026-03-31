c16e0b734a5f99acb65211316b5e8be366d69607
Daria Sapkina <124055391+DariaSapkina@users.noreply.github.com>
2026-03-31 12:54:56 +0300

    Add header

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,5 @@
 import type { FC } from "react";
-import { Header } from "../components/Header";
+import { Header } from "../components/Header/Header";
 import { Footer } from "../components/Footer";
 import type { ILayoutProps } from "./Layout.props";
 import "./Layout.scss";
