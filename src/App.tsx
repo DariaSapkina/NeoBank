77094d0cddaeaf4092683a83fbc59dffa822b925
Daria Sapkina <124055391+DariaSapkina@users.noreply.github.com>
2026-03-31 12:42:48 +0300

    Add fonts and styles to Layout and App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./router/AppRouter";
 import { Layout } from "./layout/Layout";
+import "./App.scss";
 
 function App() {
   return (
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,14 +2,15 @@ import type { FC } from "react";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import type { ILayoutProps } from "./Layout.props";
+import "./Layout.scss";
 
 const Layout: FC<ILayoutProps> = ({ children }) => {
   return (
-    <>
+    <div className="layout">
       <Header />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
